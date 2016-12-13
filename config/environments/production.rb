e4685e335b3feba4e94189aba8038ce4fbd6a127
Charles Wesley Cho <charleswcho@gmail.com>
2016-12-12 20:04:13 -0800

    Setup production version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Set up ActionCable server URI
+  config.web_socket_server_url = "wss://babble-chat.herokuapp.com/cable"
+
+  config.action_cable.allowed_request_origins = ['https://babble-chat.herokuapp.com', 'http://babble-chat.herokuapp.com']
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
