2f71f4f25ef54fbe561b8fc7f1b196bb9b3cc3e2
Charles Wesley Cho <charleswcho@gmail.com>
2016-12-16 14:37:02 -0800

    Updated migrations

--- db/migrate/20161211004630_remove_user_uniqueness.rb
+++
@@ -1,8 +0,0 @@
-class RemoveUserUniqueness < ActiveRecord::Migration[5.0]
-  def change
-    remove_index :users, :name
-    add_index :users, :name
-
-    change_column :users, :name, :string, limit: 15
-  end
-end
